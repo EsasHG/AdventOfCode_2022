3fd4ae97de28c2b7f0acbc3da59f35d76646d0ab
Espen <espenhg@hotmail.com>
2022-12-06 17:06:33 +0100

    cleaned up day 6 solution a bit

--- Day6.cs
+++ Day6.cs
@@ -3,22 +3,22 @@ class Day6
     public void Run()
     {
         string line = System.IO.File.ReadAllText("D:\\Programming\\AdventOfCode\\AdventOfCode_2022\\Inputs\\Input_day6.txt");
-        string answerPart1 = PartOne(line);
-        string answerPart2 = PartTwo(line);
+        string answerPart1 = FindFirstXDistinct(line, 4);
+        string answerPart2 = FindFirstXDistinct(line, 14);
  
         Console.WriteLine("Answer Part 1: "+ answerPart1);
         Console.WriteLine("Answer Part 2: "+ answerPart2);
     }
 
-    string PartOne(string line)
+    string FindFirstXDistinct(string line, int x)
     {
         Queue<char> q = new Queue<char>();
         int i=0;
         foreach (char c in line)
         {
-            if(q.Distinct().Count() == 4)
+            if(q.Distinct().Count() == x)
                 return i.ToString();
-            else if(q.Count == 4)
+            else if(q.Count == x)
                 q.Dequeue();
             
             q.Enqueue(c);
@@ -26,22 +26,4 @@ class Day6
         } 
         return "error";
     }
-
-    string PartTwo(string line)
-    {
-        Queue<char> q = new Queue<char>();
-        int i=0;
-        foreach (char c in line)
-        {
-            if(q.Distinct().Count() == 14)
-                return i.ToString();
-            else if(q.Count == 14)
-                q.Dequeue();
-            
-            q.Enqueue(c);
-            i++;
-        } 
-        return "error";
-    }
-
 }
